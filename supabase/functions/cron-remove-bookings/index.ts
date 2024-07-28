7f781aef907fa05ad7d7c2fded1e866f2f378b44
Kleavant James <kleavantjames@gmail.com>
2024-07-28 09:17:09 +0800

    updates env

--- supabase/functions/cron-remove-bookings/index.ts
+++ supabase/functions/cron-remove-bookings/index.ts
@@ -7,8 +7,8 @@ import "jsr:@supabase/functions-js/edge-runtime.d.ts";
 import { createClient } from "jsr:@supabase/supabase-js@2";
 
 const supabase = createClient(
-  Deno.env.get("_SUPABASE_URL")!,
-  Deno.env.get("_SUPABASE_SERVICE_KEY")!,
+  Deno.env.get("SUPABASE_URL")!,
+  Deno.env.get("SUPABASE_SERVICE_ROLE_KEY")!,
 );
 
 const dateFormatTimeZone = (yourDate: Date) => {
