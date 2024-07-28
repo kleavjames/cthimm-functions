e3ec7bebc30e5a80918ba910777788faacc205c5
Kleavant James <kleavantjames@gmail.com>
2024-07-28 09:03:56 +0800

    added functions

---
+++ supabase/functions/cron-remove-bookings/index.ts
@@ -0,0 +1,53 @@
+// Follow this setup guide to integrate the Deno language server with your editor:
+// https://deno.land/manual/getting_started/setup_your_environment
+// This enables autocomplete, go to definition, etc.
+
+// Setup type definitions for built-in Supabase Runtime APIs
+import "jsr:@supabase/functions-js/edge-runtime.d.ts";
+import { createClient } from "jsr:@supabase/supabase-js@2";
+
+const supabase = createClient(
+  Deno.env.get("_SUPABASE_URL")!,
+  Deno.env.get("_SUPABASE_SERVICE_KEY")!,
+);
+
+const dateFormatTimeZone = (yourDate: Date) => {
+  const offset = yourDate.getTimezoneOffset();
+  yourDate = new Date(yourDate.getTime() - offset * 60 * 1000);
+  return yourDate.toISOString().split("T")[0];
+};
+
+Deno.serve(async () => {
+  const nowDate = dateFormatTimeZone(new Date());
+
+  const { error, data } = await supabase.from("bookings")
+    .select("*")
+    .eq(
+      "seat_status",
+      "reserved",
+    ).eq(
+      "cancellation_date",
+      `${nowDate} 15:50:00+00`,
+    );
+
+  if (error) {
+    console.error(error);
+  }
+
+  return new Response(
+    JSON.stringify(data),
+    { headers: { "Content-Type": "application/json" } },
+  );
+});
+
+/* To invoke locally:
+
+  1. Run `supabase start` (see: https://supabase.com/docs/reference/cli/supabase-start)
+  2. Make an HTTP request:
+
+  curl -i --location --request POST 'http://127.0.0.1:54321/functions/v1/cron-remove-bookings' \
+    --header 'Authorization: Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZS1kZW1vIiwicm9sZSI6ImFub24iLCJleHAiOjE5ODM4MTI5OTZ9.CRXP1A7WOeoJeXxjNni43kdQwgnWNReilDMblYTn_I0' \
+    --header 'Content-Type: application/json' \
+    --data '{"name":"Functions"}'
+
+*/
